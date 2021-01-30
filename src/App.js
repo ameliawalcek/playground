5340a697ba0a08d8adbcf5178e2c448dea20d1b7
amelia <ameliawalcek@gmail.com>
2021-01-30 08:24:34 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect, useContext, createContext } from 'react'
+import { Task } from './Components/Task'
+
+export const TaskContext = createContext()
 
 function App() {
+
+  const [input, setInput] = useState('')
+  const [tasks, setTasks] = useState([])
+  const [id, setId] = useState(0)
+  const inputRef = useRef()
+
+  const handleChange = ({ target }) => setInput(target.value)
+
+  useEffect(() => {
+    inputRef.current.focus()
+  }, [])
+
+  const handleClick = () => {
+    setTasks([{ id, task: input }, ...tasks])
+    setId(previd => previd + 1)
+    setInput('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <TaskContext.Provider value={{ tasks, setTasks }}>
+      <div className="App">
+        <input type='text' ref={inputRef} onChange={handleChange} value={input} placeholder='to do...' />
+        <button onClick={handleClick}>New Task</button>
+
+        <div>Tasks</div>
+        {tasks.map(task => <Task task={task} key={task.id} />)}
+      </div>
+    </TaskContext.Provider>
+  )
 }
 
-export default App;
+export default App
