0e0a13a6b67293343b436fb21b36880261ebc9eb
amelia <ameliawalcek@gmail.com>
2021-01-30 08:39:17 +0200

    counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const increment = () => setCount(prevcount => prevcount + 1)
+  const decrement = () => setCount(prevcount => prevcount - 1)
 
   return (
-    <div className="App">Playground!</div>
+    <div className="App">
+      Counter
+      <div>{count}</div>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+    </div>
   )
 }
 
