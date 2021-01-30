bd296e185c7a3a77825088649bc7f83b9f27011c
amelia <ameliawalcek@gmail.com>
2021-01-30 11:17:33 +0200

    memory

--- src/App.js
+++ src/App.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { useState, useEffect, createContext, useRef } from 'react'
+import { Card } from './Componenets/Card'
+
+export const CardContext = createContext()
+export const SelectedContext = createContext()
 
 function App() {
+  const [cards, setCards] = useState([])
+  const [selected, setSelected] = useState([])
+
+  const options = [
+    { color: '#000000', active: false, match: false },
+    { color: '#2A1A1F', active: false, match: false },
+    { color: '#764134', active: false, match: false },
+    { color: '#AD8350', active: false, match: false },
+    { color: '#AFA060', active: false, match: false },
+    { color: '#B084CC', active: false, match: false },
+    { color: '#5DD9C1', active: false, match: false },
+    { color: '#ACFCD9', active: false, match: false }
+  ]
+
+  const shuffle = array => array.sort(() => Math.random() - 0.5)
+
+  const createCards = () => {
+    let doubles = shuffle([...options, ...options])
+      .map(double => ({ ...double, id: Math.random() }))
+
+    setCards(doubles)
+  }
+
+  useEffect(() => {
+    createCards()
+  }, [])
+
+  const isMatch = () => new Set(selected).size !== selected.length
+
+  if (selected.length === 2) {
+    if (isMatch()) {
+      console.log('match')
+      const updateCards = cards.map(c => {
+        return c.color === selected[0]
+          ? { ...c, active: false, match: true }
+          : c
+      })
+      setCards(updateCards)
+      
+    } else {
+      const reset = cards.map(card => ({ ...card, active: false }))
+      setCards(reset)
+    }
+    setSelected([])
+  }
 
   return (
-    <div className="App">Playground!</div>
+    <CardContext.Provider value={{ cards, setCards }}>
+      <SelectedContext.Provider value={{ selected, setSelected }}>
+        <div className="App">Memory game
+      <div className='board'>
+            {cards.map(card => <Card card={card} key={Math.random()} />)}
+          </div>
+        </div>
+      </SelectedContext.Provider>
+    </CardContext.Provider>
   )
 }
 
---
+++ src/Componenets/Card.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useContext } from 'react'
+import { CardContext, SelectedContext } from '../App.js'
+
+export const Card = ({ card }) => {
+    const { cards, setCards } = useContext(CardContext)
+    let { selected, setSelected } = useContext(SelectedContext)
+
+    const handleClick = () => {
+        const updateCards = cards.map(c => {
+            return c.id === card.id
+                ? { ...c, active: true }
+                : c
+        })
+        setCards(updateCards)
+        setSelected([...selected, card.color])
+    }
+
+    return (
+        <div>
+            <div
+                style={{
+                    height: '50px',
+                    width: '50px',
+                    backgroundColor: card.active ? card.color : 'grey',
+                }}
+                onClick={handleClick}
+            >
+            </div>
+        </div>
+    )
+}
