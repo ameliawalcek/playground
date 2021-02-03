c9f6f826931bf417b176edc227b20cf055b0fe78
amelia <ameliawalcek@gmail.com>
2021-02-03 17:52:36 +0200

    other branches

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from 'react'
 function App() {
 
   return (
-    <div className="App">Playground!</div>
+    <div className="App">Playground!
+      <div>Check out other branches to see more</div>
+    </div>
   )
 }
 
